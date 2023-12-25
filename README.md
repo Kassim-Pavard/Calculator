ec41ba9ae7444bfef0008d6e9a570105cf4036ee
Kasuop <kassim_782@yahoo.fr>
2023-12-25 02:21:21 +0100

    final commit - script.js

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Calculator
+
+## Description
+
+Petite calculatrice très modeste mais quand même sympa.
+
+## Table des Matières
+
+- [Installation](#installation)
+- [Utilisation](#utilisation)
+- [Capture d'écran](#capture-décran)
+- [Contacts](#contacts)
+
+## Installation
+
+1. Clonez ce dépôt.
+2. C'est tout :)
+
+## Utilisation
+
+- Ouvrir le fichier index.html avec votre navigateur préféré.
+
+## Capture d'écran
+
+![Capture d'écran de l'application](calculator.png)
+
+## Contacts
+
+- Auteur : [Kassim Pavard](https://github.com/Kassim-Pavard)
+- Email : kassim_782@yahoo.fr
---
+++ script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll('input[type="button"]');
+const display = document.querySelector("#display");
+console.log(buttons);
+console.log(display);
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    switch (this.value) {
+      case "AC":
+        // clear display
+        display.value = "";
+        break;
+      case "DE":
+        // delete last digit
+        display.value = display.value.toString().slice(0, -1);
+        break;
+      case "=":
+        try {
+          // eval to calculate
+          display.value = eval(display.value);
+        } catch (error) {
+          // error case
+          display.value = "Error";
+        }
+        break;
+      default:
+        // add the button value next to the value already on display
+        display.value += this.value;
+    }
+  });
+});
